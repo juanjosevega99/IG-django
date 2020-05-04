e26bc71c6a8e3de202f05ebb6d1d5686abc4e9f7
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-04 17:38:19 -0500

    first commit

---
+++ .env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\juanj\desktop\platzi\2020\django\platzigram\.env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
