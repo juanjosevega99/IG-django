9591e0573caba28e2c766512a0fcdb4dfb513401
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2020-05-05 09:05:53 -0500

    args in URL

--- platzigram/views.py
+++ platzigram/views.py
@@ -3,18 +3,30 @@ from django.http import HttpResponse
 from datetime import datetime
 import json
 
+
 def hello_world(request):
     return HttpResponse('Hello, world! {now}'.format(
         now=datetime.now().strftime('%b %dth, %y - %H:%M hrs')
     ))
 
 
-def hi(request):
-    numbers = map(int, request.GET['numbers'].split(','))
-    numbers = json.dumps(dict(enumerate(sorted(numbers))))
-    # pdb.set_trace()
-
+def sort_integers(request):
+    numbers = [int(i) for i in request.GET['numbers'].split(',')]
+    sorted_ints = sorted(numbers)
+    data = {
+        'status': 'ok',
+        'numbers': sorted_ints,
+        'status': 'Integers sorted successfully'
+    }
     return HttpResponse(
-        numbers,
+        json.dumps(data, indent=4),
         content_type='application/json'
     )
+
+
+def say_hi(request, name, age):
+    if age < 12:
+        message = 'Sorry {}, you are not allowed here'.format(name)
+    else:
+        message = 'Hello, {}! Welcome to Platzigram'.format(name)
+    return HttpResponse(message)
